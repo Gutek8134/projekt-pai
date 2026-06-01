3b165e83b6b1885be6bdf0159851eeeb8d1125be
Dominik Gołata <gutek8134@gmail.com>
2026-06-01 10:08:25 +0200

    format models.py

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,18 +1,24 @@
+from django.db.models.manager import Manager
 from django.db import models
 
 
 # Create your models here.
 class RestaurantMenuType(models.Model):
+    objects: Manager
     menu_type = models.CharField(max_length=50, verbose_name="Menu Type")
 
     def __str__(self):
         return repr(self.menu_type)
 
+
 class Restaurant(models.Model):
+    objects: Manager
     name = models.CharField(max_length=100, verbose_name="Restaurant Name")
-    menu_type = models.ForeignKey(RestaurantMenuType, on_delete=models.RESTRICT)
-    address = models.CharField(max_length=255, verbose_name="Restaurant Address")
-    photo = models.ImageField(width_field=512, height_field=512)
+    menu_type = models.ForeignKey(
+        RestaurantMenuType, on_delete=models.RESTRICT)
+    address = models.CharField(
+        max_length=255, verbose_name="Restaurant Address")
+    photo = models.ImageField(width_field="512", height_field="512")
     latitude = models.DecimalField(
         max_digits=8, decimal_places=5, verbose_name="Restaurant Latitude"
     )
@@ -23,7 +29,9 @@ class Restaurant(models.Model):
     def __str__(self):
         return repr(self.name)
 
+
 class Review(models.Model):
+    objects: Manager
     for_restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     rating = models.IntegerField(verbose_name="Rating")
     comment_text = models.TextField(verbose_name="Review Details")
